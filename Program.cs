efe9902fae6bb3df9eab59da6c29b466257e0a07
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-09-12 21:41:08 +0300

    develop

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.Out.WriteLine("hello world!!!");
+            Console.Out.WriteLine("what is your name?");
+            String name = Console.In.ReadLine();
+            Console.Out.WriteLine("Ok. " + name);
             Console.In.Read();
         }
     }
